7f9565d65a16f380fc59b1574ddb33ea052ca9f9
johnfernandis1999 <140940560+johnfernandis1999@users.noreply.github.com>
2024-08-06 21:34:52 +0530

    file Commited

--- 12-WeatherApp/src/App.jsx
+++ 12-WeatherApp/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() =>{
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        fetch(`https:api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=7aa2b960ec0010c47e78e236df746402
+`)
+        .then((response) => response.json())
+        .then((data)=> setWeather(data));
+        // .then((data) => console.log(data));
+      })
+    }
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+   <div>
+    {
+      weather ? (
+        <div>
+         <h2>Current Weather</h2>
+            <p>Tempreature : {weather.main.temp}</p>
+            <p>Tempreature : {weather.weather[0].description}</p>
+        </div>
+      ) : <p>
+        Looding....
       </p>
-    </>
+    }
+   </div>
   )
 }
 
